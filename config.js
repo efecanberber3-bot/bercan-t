7ae2dc964f9f66c9350963f0f5beb718e3cfc152
Bercant <efecanberber3@gmail.com>
2026-06-15 17:26:08 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.BERCANT_CONFIG = {
+  SUPABASE_URL: "https://jgxebzxsimbemzzobpmh.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_U3TnltKNw-3_ekm68Co8eg__lrYVjkb",
+  DEMO_MODE: false,
+  SITE_URL: "https://efecanberber3-bot.github.io/demo",
+  SUPPORT_EMAIL: "yenelberkant@gmail.com"
+};
