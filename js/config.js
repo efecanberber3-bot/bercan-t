96cc033f58fe1a8482b88726d21314720210084b
Bercant <efecanberber3@gmail.com>
2026-06-15 17:27:06 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ window.BERCANT_CONFIG = {
   SUPABASE_URL: "https://jgxebzxsimbemzzobpmh.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_U3TnltKNw-3_ekm68Co8eg__lrYVjkb",
   DEMO_MODE: false,
-  SITE_URL: "https://efecanberber3-bot.github.io/demo",
+  SITE_URL: "https://bercant.com.tr/",
   SUPPORT_EMAIL: "yenelberkant@gmail.com"
 };
